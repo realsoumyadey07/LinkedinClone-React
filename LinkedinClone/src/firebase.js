68ebb5f0ac6541ce4e5b729fad10ff406c894c35
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-01-18 22:47:52 +0530

    on going

---
+++ LinkedinClone/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9uVbjioaO3uGk2s8GOS0D2qEFpOKRxRo",
+  authDomain: "linkedinclone-bfa9a.firebaseapp.com",
+  projectId: "linkedinclone-bfa9a",
+  storageBucket: "linkedinclone-bfa9a.appspot.com",
+  messagingSenderId: "844816100975",
+  appId: "1:844816100975:web:98077f9e766fcd66d6279a"
+};
+
+
+export const app = initializeApp(firebaseConfig);
