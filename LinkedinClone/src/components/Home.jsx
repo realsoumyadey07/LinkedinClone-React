e585e64ab0e6096a3d6bdf782911a5e392fbb298
Soumyadip Dey <141472667+realsoumyadey07@users.noreply.github.com>
2024-01-22 12:23:31 +0530

    on going

--- LinkedinClone/src/components/Home.jsx
+++ LinkedinClone/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
+import LeftSide from './LeftSide';
+import Main from './Main';
+import RightSide from './RightSide';
 function Home(props) {
   return (
     <Container>
@@ -11,6 +14,11 @@ function Home(props) {
           Find talented pros in record time with Upwork and keep business moving.
         </p>
       </Section>
+      <Layout>
+        <LeftSide/>
+        <Main/>
+        <RightSide/>
+      </Layout>
     </Container>
   )
 }
@@ -54,4 +62,18 @@ const Section = styled.section`
     }
   }
   
+`
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+  @media (max-width: 768px){
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
 `
---
+++ LinkedinClone/src/components/RightSide.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function RightSide() {
+  return (
+    <Container>Right Side</Container>
+  )
+}
+
+export default RightSide
+
+const Container = styled.div`
+  grid-area: rightside;
+`
